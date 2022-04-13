7e1623bdb43af85f7ca33fd10bbfcf7a24dfb512
officialpedrocarvalho <pedrocarvalho@gmail.com>
2022-04-13 17:10:07 +0100

    Fix problem with set-cookie session id in browser

--- CollectDataService/CollectDataAPI/views.py
+++ CollectDataService/CollectDataAPI/views.py
@@ -36,16 +36,15 @@ class WebPageViewSet(ModelViewSet):
     serializer_class = WebPageSerializer
 
     def create(self, request):
-        print(request.session.session_key)
-        if not request.session.exists(request.session.session_key):
-            request.session.create()
-        session = Session.objects.get(session_key=request.session.session_key)
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         url = split_by_character_in_position(serializer.validated_data.get('url'), "/", 3)
         domain = Domain.objects.filter(domain=url).first()
         if domain:
             web_site = domain.webSite
+            if not request.session.exists(request.session.session_key):
+                request.session.create()
+            session = Session.objects.get(session_key=request.session.session_key)
             serializer.save(session=session, webSite=web_site)
             headers = self.get_success_headers(serializer.data)
             return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
